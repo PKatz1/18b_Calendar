2ff9734a9544920f2bde7c94d62e36a10c2f1945
PKatz1 <77889978+PKatz1@users.noreply.github.com>
2021-06-03 17:51:06 -0400

    adding google calendar

---
+++ calendar.js
@@ -0,0 +1,7 @@
+var p = MindFusion.Scheduling;
+
+//create a new calendar instance
+var calendar = new p.Calendar(document.getElementById("calendar"));
+
+//visualize the calendar
+calendar.render();
